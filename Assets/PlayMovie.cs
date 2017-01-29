605e92eb630475abf2255fd93919a719e3de1094
Yiwen Zhang <tiotaocn@gmail.com>
2017-01-29 16:47:33 -0500

    update andorid settings

--- Assets/PlayMovie.cs
+++ Assets/PlayMovie.cs
@@ -5,14 +5,18 @@ public class PlayMovie : MonoBehaviour {
 	public float counter=20.0f;
 	// Use this for initialization
 	void Start () {
-		MovieTexture mov;
+        #if UNITY_ANDROID
+            Handheld.PlayFullScreenMovie("Sprites/FinalAnimation.mp4");
+        #else
+            MovieTexture mov;
+            mov = (MovieTexture)this.GetComponent<Renderer> ().material.mainTexture;
+		    mov.Play ();
+        #endif
 
-		mov = (MovieTexture)this.GetComponent<Renderer> ().material.mainTexture;
-		mov.Play ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update () {
 		counter -= Time.deltaTime;
 		if (counter <= 0) {
 			SceneManager.LoadScene ("Begin");
