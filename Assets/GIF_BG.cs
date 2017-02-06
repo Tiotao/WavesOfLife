33103664e2ffe871776b466e72aac62cfbf21be5
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-05 19:17:32 -0500

    add level one

--- Assets/GIF_BG.cs
+++ Assets/GIF_BG.cs
@@ -5,7 +5,9 @@ public class GIF_BG : MonoBehaviour {
 	public Sprite[] BG;
 	private int i=0;
 	private float counter = 1 / 12;
-	private float Begin = 39.0f;
+    private float Begin = 39.0f;
+  //  private float Begin = 0f;
+    private bool reverse;
 	// Use this for initialization
 	void Start () {
 		BG = Resources.LoadAll<Sprite> ("BGanimation");
@@ -18,13 +20,26 @@ public class GIF_BG : MonoBehaviour {
 		if (Begin <= 0) {
 			counter -= Time.deltaTime;
 			if (counter <= 0) {
-			
-				this.GetComponent<SpriteRenderer> ().sprite = BG [i];
-				i++;
-				counter = 1 / 12;
-				if (i >= 90) {
-					i = 0;
+
+                if (!reverse)
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = BG[i];
+                    i++;
+                }
+                else
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = BG[i];
+                    i--;
+                }
+                    counter = 1 / 12;
+                
+				if (i >= 45) {
+                    reverse = true;
 				}
+                if(i<=0)
+                {
+                    reverse = false;
+                }
 			}
 		}
 	}
