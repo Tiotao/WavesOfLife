24381ee8cb540e27ea91060b21f64f400e39cb63
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-05 19:48:45 -0500

    upload sprites

---
+++ Assets/BGanimationLvl1.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGanimationLvl1 : MonoBehaviour {
+    public Sprite[] BG;
+    private int i = 0;
+    private float counter = 1 / 12;
+  //  private float Begin = 39.0f;
+      private float Begin = 0f;
+    private bool reverse;
+    // Use this for initialization
+    void Start()
+    {
+        BG = Resources.LoadAll<Sprite>("level1_animation");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Begin > 0)
+            Begin -= Time.deltaTime;
+        if (Begin <= 0)
+        {
+            counter -= Time.deltaTime;
+            if (counter <= 0)
+            {
+
+                if (!reverse)
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = BG[i];
+                    i++;
+                }
+                else
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = BG[i];
+                    i--;
+                }
+                counter = 1 / 12;
+
+                if (i >= 45)
+                {
+                    reverse = true;
+                }
+                if (i <= 0)
+                {
+                    reverse = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/PlayerNear.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNear : MonoBehaviour {
+    public Sprite Open;
+    public Sprite Close;
+    private SpriteRenderer _render;
+	// Use this for initialization
+	void Start () {
+        _render = this.GetComponent<SpriteRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            _render.sprite = Open;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _render.sprite = Close;
+        }
+    }
+}
