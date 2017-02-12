586c04788a427671125ea169ee035fc5ed809247
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-12 15:33:35 -0500

    Enemy Fish Added

---
+++ Assets/TriggerEnemyFish.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerEnemyFish : MonoBehaviour {
+    public GameObject EnemyFish;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            EnemyFish.SetActive(true);
+          //  EnemyFish.GetComponentInChildren<SpriteRenderer>().enabled = true;
+            EnemyFish.GetComponent<Animator>().SetTrigger("Play");
+        }
+    }
+}
