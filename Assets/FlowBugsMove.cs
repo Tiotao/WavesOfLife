45b3b57de45e957c1dcd9e632a69be89e3d625d6
Haoqing Luan <hluan@andrew.cmu.edu>
2017-02-19 22:56:04 -0500

    modified ending animation

---
+++ Assets/FlowBugsMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowBugsMove : MonoBehaviour {
+    public float Speed;
+    public bool BeginMove;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(BeginMove)
+        this.transform.Translate(new Vector3(0, -1, 0) * Speed * Time.deltaTime * 60f);
+    }
+}
