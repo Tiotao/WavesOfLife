09e7f09397268747614ab5b6365132336432a65d
Haoqing Luan <hluan@andrew.cmu.edu>
2017-03-05 21:13:51 -0500

    modified codes to add begin and end maze

---
+++ Assets/EndAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndAnimation : MonoBehaviour {
+
+    public Sprite[] BG;
+    public string _spriteFolderName = "lvl1_deadfish";
+    public int _frameNumber = 45;
+    public int _frameRate = 6;
+    private int i = 0;
+    private float counter;
+    public bool startplay;
+    // Use this for initialization
+    void Start()
+    {
+        BG = Resources.LoadAll<Sprite>(_spriteFolderName);
+        counter = 1 / _frameRate;
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        if (startplay)
+        { 
+                counter -= Time.deltaTime;
+                if (counter <= 0)
+                {
+                    if (i < BG.Length)
+                    {
+                        this.GetComponent<SpriteRenderer>().sprite = BG[i];
+                        i++;
+                        counter = 1f / _frameRate;
+                        //    Debug.Log(i);
+                    }
+                    else
+                    {
+                        this.enabled = false;
+                    }
+                }
+            
+        }
+    }
+}
