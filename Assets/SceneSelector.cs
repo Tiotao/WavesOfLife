69c6b56af4c0424b515fe3b6eb17204ea9092414
ANDREW\jiajunl2 <liujj312@qq.com>
2017-03-12 19:23:51 -0400

    add level unlock system

--- Assets/SceneSelector.cs
+++ Assets/SceneSelector.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Xml;
+
 
 public class SceneSelector : MonoBehaviour {
 
@@ -11,7 +13,6 @@ public class SceneSelector : MonoBehaviour {
 	public AsyncSceneLoader _AsyncSceneLoader;
 	private int _sceneIndexOffset = 1;
 	
-
 	public int _maxLevel = 2;
 
 
@@ -23,6 +24,10 @@ public class SceneSelector : MonoBehaviour {
 			_SceneBtns[i].gameObject.GetComponent<Image>().sprite = _SceneUnlockBtnsSprite[i];
 			_SceneBtns[i].onClick.AddListener(delegate () { LoadScene(_sceneIndex); });
 		}
+
+        // try unlock 2nd level.
+        LoadLevels();
+        //SetLevel("1", true);
 		
 	}
 	
@@ -40,4 +45,70 @@ public class SceneSelector : MonoBehaviour {
 		yield return new WaitForSeconds (3f);
         _AsyncSceneLoader.ToSelectedScene(sceneIndex);
     }
+
+    // The following two functions will maintain level list and set the lock/unlock state of a level.
+
+    // Load Xml file.
+    public static List<Level> LoadLevels()
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        string filePath = Application.persistentDataPath + "/levels.xml";
+        if (!IOHelper.isFileExists(filePath))
+        {
+            xmlDoc.LoadXml(((TextAsset)Resources.Load("levels")).text);
+            IOHelper.CreateFile(filePath, xmlDoc.InnerXml);
+        }
+        else
+        {
+            xmlDoc.Load(filePath);
+        }
+        XmlElement root = xmlDoc.DocumentElement;
+        XmlNodeList levelsNode = root.SelectNodes("/levels/level");
+        // make the level list
+        List<Level> levels = new List<Level>();
+        foreach (XmlElement xe in levelsNode)
+        {
+            Level l = new Level();
+            l.ID = xe.GetAttribute("id");
+            // mark unlock
+            if (xe.GetAttribute("unlocked") == "1")
+            {
+                l.Unlocked = true;
+            } else
+            {
+                l.Unlocked = false;
+            }
+            levels.Add(l);
+            }
+        return levels;
+        }
+
+    public static void SetLevel(string ID, bool unlock)
+    {
+        // create the xml object
+        XmlDocument xmlDoc = new XmlDocument();
+        string filePath = Application.persistentDataPath + "/levels.xml";
+        xmlDoc.Load(filePath);
+        XmlElement root = xmlDoc.DocumentElement;
+        XmlNodeList levelsNode = root.SelectNodes("/levels/level");
+
+        //find the corresponding level
+        foreach (XmlElement xe in levelsNode){
+            if (xe.GetAttribute("id") == ID)
+            {
+                if (unlock)
+                {
+                    xe.SetAttribute("unlocked", "1");
+                }
+                else
+                {
+                    xe.SetAttribute("unlocked", "0");
+                }
+            }
+        }
+        //save the file
+        xmlDoc.Save(filePath);
+    }
 }
+
+
---
+++ Assets/Script/IOHelper.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Level
+{
+    public string ID;
+    public bool Unlocked = false;
+}
+
+public static class IOHelper
+{
+    // 创建文件夹    
+    /// <param name="path">文件夹路径</param>
+    public static void CreateFolder(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+
+    // 创建文件
+    /// <param name="filePath">文件路径</param>
+    /// <param name="content">文件内容</param>
+    public static void CreateFile(string filePath, string content)
+    {
+        //文件流
+        StreamWriter writer;
+        //判断文件目录是否存在
+        //不存在则先创建目录
+        Debug.Log(filePath);
+        string folder = filePath.Substring(0, filePath.LastIndexOf("/"));
+        Debug.Log(folder);
+        CreateFolder(folder);
+        //如果文件不存在则创建，存在则追加内容
+        FileInfo file = new FileInfo(filePath);
+        if (!file.Exists)
+        {
+            writer = file.CreateText();
+        }
+        else
+        {
+            file.Delete();
+            writer = file.CreateText();
+        }
+
+        //写入内容
+        writer.Write(content);
+        writer.Close();
+        writer.Dispose();
+    }
+
+    // 判断文件是否存在
+    // <param name="path">文件路径</param>
+    public static bool isFileExists(string path)
+    {
+        FileInfo file = new FileInfo(path);
+        return file.Exists;
+    }
+
+    public static void DeleteFile(string fileName)
+    {
+        if (!File.Exists(fileName)) return;
+        File.Delete(fileName);
+    }
+
+}
