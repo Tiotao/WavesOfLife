53d75471d5908f4af1ffd7cbd7bd8d64a0643ea3
Haoqing Luan <hluan@andrew.cmu.edu>
2017-03-26 23:32:07 -0400

    added animationssssss

---
+++ Assets/EndAnimationlvl2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndAnimationlvl2 : MonoBehaviour {
+    public GameObject SnakeHead;
+    public GameObject Startposition;
+    public GameObject Player;
+    public GameObject cam;
+    public bool automove;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(automove)
+        {
+            Player.transform.Translate(new Vector3(0, 0, 1) * 0.04f * Time.deltaTime * 60f);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SnakeHead.GetComponent<SpritesAnimation>().Begin = -1;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<AutoMove>().lvl1End = true;
+            Invoke("AfterEndAni", 2.0f);
+        }
+    }
+    private void AfterEndAni()
+    {
+        Player.transform.position = Startposition.transform.position;
+        cam.transform.eulerAngles = new Vector3(0, 0, -90f);
+        Player.transform.eulerAngles = new Vector3(-180f, -90f, 0);
+        automove = true;
+    }
+
+
+
+}
