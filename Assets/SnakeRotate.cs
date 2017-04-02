a83d7726f7fd9a31946754e1fb1695bdc2710b66
Haoqing Luan <hluan@andrew.cmu.edu>
2017-04-02 17:34:06 -0400

    snake and bugs

---
+++ Assets/SnakeRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeRotate : MonoBehaviour {
+    public bool CountDirection;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!CountDirection)
+            this.transform.Rotate(new Vector3(0, 0, 4f * Time.deltaTime));
+        else
+            this.transform.Rotate(new Vector3(0, 0, -4f * Time.deltaTime));
+    }
+}
