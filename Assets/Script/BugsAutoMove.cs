7374d5f689b82abf9db4d1fe0bf0e122549e04db
Yiwen Zhang <tiotaocn@gmail.com>
2017-04-02 20:36:15 -0400

    update insects

--- Assets/Script/BugsAutoMove.cs
+++ Assets/Script/BugsAutoMove.cs
@@ -10,47 +10,73 @@ public class BugsAutoMove : MonoBehaviour {
     private bool BeginRotate=false;
     private float RanDirection;
     private Vector3 InitRotation;
+    
+
+    
+    public Transform _endingPt;
+    public Transform _startingPt;
+
+    public Transform _midwayPt;
+
+    
 	// Use this for initialization
 	void Start () {
+        _endingPt = GameObject.Find("InsectsEndPoint").transform;
+        _startingPt = GameObject.Find("InsectsSpawnPoint").transform;
+        _midwayPt = GameObject.Find("InsectsSpawnPoint").transform;
+        transform.position = new Vector3(_startingPt.position.x + Random.Range(-5f, 5f), _startingPt.position.y + Random.Range(0f, 1f), _startingPt.position.z);
+        Vector3 randomizedEnding = new Vector3(_endingPt.position.x + Random.Range(-2f, 2f), _endingPt.position.y + Random.Range(0f, 1f), _endingPt.position.z);
+        Vector3 randomizedMidway = new Vector3(_midwayPt.position.x + Random.Range(-1f, 1f), _midwayPt.position.y + Random.Range(0f, 0f), _midwayPt.position.z);
+
+        iTween.MoveTo(this.gameObject, iTween.Hash("path", new Vector3[]{randomizedMidway, randomizedEnding} , "time", Random.Range(6f, 8f), "oncomplete", "DestroyInsect", "movetopath", true, "orienttopath",true, "easetype", iTween.EaseType.linear));
+
         float rand = Random.Range(0, 4);
+        float randomSizeFactor = Random.Range(1f, 1.5f);
+        this.GetComponentInChildren<SpriteRenderer>().transform.localScale = new Vector3(randomSizeFactor, randomSizeFactor, randomSizeFactor);
         if (rand <= 2)
             this.GetComponentInChildren<SpriteRenderer>().sprite = P1;
         else 
         {
             this.GetComponentInChildren<SpriteRenderer>().sprite = P2;
-            _sprite.transform.eulerAngles = new Vector3(0,0,-90f);
+            // _sprite.transform.eulerAngles = new Vector3(0,0,-90f);
         }
-        rand = Random.Range(1,2);
-        this.transform.localScale = new Vector3(rand,rand,rand);
-        RanDirection = Random.Range(-30, 30);
-        InitRotation = this.transform.eulerAngles;
+        // rand = Random.Range(0.5f,1f);
+        // this.transform.localScale = new Vector3(rand,rand,rand);
+        // RanDirection = Random.Range(-10, 10);
+        // InitRotation = this.transform.eulerAngles;
+    }
+
+    void DestroyInsect () {
+        Destroy(gameObject);
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        
       
-        if (!TimeStop)
-            this.transform.Translate(Vector3.up * Time.deltaTime*60f*Speed);
+        // if (!TimeStop)
+        //     this.transform.Translate(Vector3.up * Time.deltaTime*60f*Speed);
 
-        timer += Time.deltaTime;
-        if(timer>=0.5f)
-        {
-            RanDirection = Random.Range(-30, 30);
+        // timer += Time.deltaTime;
+        // if(timer>=0.5f)
+        // {
+        //     RanDirection = Random.Range(-10, 10);
 
-            InitRotation = this.transform.eulerAngles;
-            timer = 0;
-        }
+        //     InitRotation = this.transform.eulerAngles;
+        //     timer = 0;
+        // }
 
-        if (Mathf.Abs(this.transform.eulerAngles.z - InitRotation.z) < Mathf.Abs(RanDirection))
-        {
-            if(RanDirection>=0)
-                this.transform.Rotate(new Vector3(0, 0, 1));
-            else
-            {
-                this.transform.Rotate(new Vector3(0, 0, -1));
-                Debug.Log("-1");
-            }
-        }
+        // if (Mathf.Abs(this.transform.eulerAngles.z - InitRotation.z) < Mathf.Abs(RanDirection))
+        // {
+        //     if(RanDirection>=0)
+        //         this.transform.Rotate(new Vector3(0, 0, 1));
+        //     else
+        //     {
+        //         this.transform.Rotate(new Vector3(0, 0, -1));
+        //         Debug.Log("-1");
+        //     }
+        // }
 
     }
 }
