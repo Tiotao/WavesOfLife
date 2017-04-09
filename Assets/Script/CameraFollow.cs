c6353aa076bbf0fa8d511d00c4bb4317dfe115ae
Haoqing Luan <hluan@andrew.cmu.edu>
2017-04-09 15:35:25 -0400

    Modified camera follow

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -9,17 +9,21 @@ public class CameraFollow : MonoBehaviour {
 	Vector3 offset;
 	float AngleOffset;
 	private Vector3 velocity = Vector3.zero;
-
-	// Use this for initialization
-	void Start () {
+    Vector3 targetCamPosition;
+    // Use this for initialization
+    void Start () {
 		offset = transform.position - targetPosition.position;
 	//	AngleOffset = transform.eulerAngles.z - targetPosition.eulerAngles.x;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Vector3 targetCamPosition = targetPosition.position +  (offset.x)*targetPosition.forward;
-		targetCamPosition.z = this.transform.position.z;
+       // Debug.Log(offset.y);
+       if(Mathf.Abs(offset.x)>Mathf.Abs(offset.y))
+            targetCamPosition = targetPosition.position +  (offset.x)*targetPosition.forward;
+       else
+            targetCamPosition = targetPosition.position + (offset.y) * targetPosition.forward;
+        targetCamPosition.z = this.transform.position.z;
 	//	Vector3 targetCamEu = transform.eulerAngles;
 	//	targetCamEu.z = AngleOffset + targetPosition.eulerAngles.x-transform.eulerAngles.z;
 		// smoothdamp the camera movement
