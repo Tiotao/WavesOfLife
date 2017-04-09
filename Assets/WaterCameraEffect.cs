6340d18c47aab62ea56b4923f8e0b0e72a1ffe6f
Zhang Yiwen <yiwen@pigeonlab.com>
2017-04-08 22:23:21 -0400

    change sprite animation to shader

---
+++ Assets/WaterCameraEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class WaterCameraEffect : MonoBehaviour {
+
+	// Use this for initialization
+	public Material mat;
+
+	/// <summary>
+	/// OnRenderImage is called after all rendering is complete to render image.
+	/// </summary>
+	/// <param name="src">The source RenderTexture.</param>
+	/// <param name="dest">The destination RenderTexture.</param>
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		Graphics.Blit(src, dest, mat);
+	}
+}
